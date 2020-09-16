6550b37fa592648e869a5e913bcd4a5f010e3178
korks2931 <korks2931@wrdsb.ca>
2020-09-16 01:38:35 +0000

    finished a2q5

--- Main.java
+++ Main.java
@@ -1,15 +1,43 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * This program will tell the user what to say depending on the number they have entered
+ * @author Sevde
  */
 public class Main {
 
   /**
    * The method that is executed when you hit the run button.
+   * 
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create a Scanner for user input
+    Scanner input = new Scanner(System.in);
+    System.out.println("Please enter a number to play FizzBuzz");
+    // prompt the user for the number
+    int number = input.nextInt();
+    // declare a variable to store remainderone
+   
+    int remainderone = (number % 3);
+    if (remainderone == 0){
+      System.out.println("You should say Fizz");
+    }
+    // declare a variable to store remaindertwo
+      int remaindertwo = (number % 5);
+    if (remaindertwo == 0){
+      System.out.println("You should say Buzz");
+      
+    }
+  
+    if (remainderone == 0 && remaindertwo == 0){
+      System.out.println("You should say FizzBuzz ");
+
+    } else if (remainderone > 0 && remaindertwo > 0){
+      System.out.println("You should say " + number);
+    }
   }
-}
+
+  }
+
+
